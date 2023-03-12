49a44acb45b95ef820caa3f1622682d7de9e3f46
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-12 20:28:37 +0700

    fastcrud installed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuditController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\CaptchaServiceController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UploadController;
+use App\Http\Controllers\UserController;
+// URL_CRUD_GENERATOR
+use App\Http\Controllers\CrudController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +25,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/reload-captcha', [CaptchaServiceController::class, 'reloadCaptcha']);
+Route::post('upload/store', [UploadController::class, 'store'])->name('upload.store');
+Route::delete('revert/image', [UploadController::class, 'revert'])->name('upload.revert');
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::resource('permission', PermissionController::class);
+    Route::resource('role', RoleController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('audit', AuditController::class);
+    // CRUD_GENERATOR
+    Route::resource('crud', CrudController::class);
+    Route::get('user/resetPassword/{user}', [UserController::class, 'reset_password'])->name('user.resetPassword');
+});
+require __DIR__ . '/auth.php';
